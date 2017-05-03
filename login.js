0f3ab69af5da037cab5d4e6bd1327a9d00e47150
niklasbolwede <niklasbolwede@gmail.com>
2017-05-03 13:11:01 +0200

    Login script validation start Niklas, Gual

--- login.js
+++ login.js
@@ -1,3 +1,16 @@
 /**
  * Created by niklasbolwede on 2017-05-03.
  */
+var app = angular.module('app', ["ui.router"]);
+
+
+
+app.controller('IndexController', function($scope, $rootScope) {
+    var email= "a@a.com";
+    var password = "1234";
+    if ($login.email === email && $login.password === password){
+        alert("Success!");
+    }else {
+        alert("Fail!");
+    }
+});
