17a017ad02842baa9402536d7b983b7fec750424
niklasbolwede <niklasbolwede@gmail.com>
2017-05-04 15:38:58 +0200

    Validation function

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,8 +5,34 @@
 angular.module('app').controller('loginController', function($scope, $rootScope) {
     var email= "a@a.com";
     var password = "1234";
-   
-    function loginClicked() {
 
+    function loginClicked() {
+        if(LoginAuthentication.login($scope.loginForm.email, $scope.loginForm.password)) {
+            $scope.error = '';
+            $scope.username = '';
+            $scope.password = '';
+            $state.transitionTo('chat');
+        } else {
+            alert("Incorrect!")
+        }
     }
+});
+
+angular.module('app').factory('LoginAuthentication', function() {
+    var email = 'a@a.com';
+    var password = '1234';
+    var isAuthenticated = false;
+
+    return {
+        login : function(inputEmail, inputPassword) {
+            if (email === inputEmail && password === inputPassword){
+                isAuthenticated = true;
+            }
+            return isAuthenticated;
+        }
+        /*isAuthenticated : function() {
+         return isAuthenticated;
+         }*/
+    };
+
 });
