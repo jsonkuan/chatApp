fa46cb4ea1a90a82220d6f33646bc915d77d5dcd
Dervis <dervis85@gmail.com>
2017-05-04 11:06:00 +0200

    typos2

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,25 +1,12 @@
 /**
  * Created by niklasbolwede on 2017-05-03.
  */
-<<<<<<< HEAD
-var app = angular.module('app', ["ui.router"]);
 
-
-
-app.controller('IndexController', function($scope, $rootScope) {
-    var email= "a@a.com";
-    var password = "1234";
-    if ($login.email === email && $login.password === password){
-        alert("Success!");
-    }else {
-        alert("Fail!");
-=======
 angular.module('app').controller('loginController', function($scope, $rootScope) {
     var email= "a@a.com";
     var password = "1234";
    
     function loginClicked() {
 
->>>>>>> origin/login
     }
 });
