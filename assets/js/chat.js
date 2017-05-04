644078821c9b5a1fdb71f101b3f0dc4cdff2f548
Jason Kuan <jsonkuan@gmail.com>
2017-05-04 15:58:39 +0200

    ng-repeat to populate lists

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,13 +1,6 @@
-
-
 angular.module('app').controller('chatController', function($scope) {
-
     $scope.channels = ['General', 'Work', 'Afterwork', 'Crazy cat videos', 'pr0n'];
-
     $scope.contacts = ['Snygg-Kuan', 'Cool-boy-Scolari', 'Papa-Niklas', 'Super-Jakob', 'Nerd-Dervish', 'Killer-Christian'];
-
-    //Test
-
 });
 
 
