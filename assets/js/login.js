65e578464fe3d2691349e6dcb4058a7a3a25b98d
guasc0 <gual.scolari@hotmail.com>
2017-05-04 10:46:15 +0200

    login validation and some design

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,16 +1,11 @@
 /**
  * Created by niklasbolwede on 2017-05-03.
  */
-var app = angular.module('app', ["ui.router"]);
-
-
-
-app.controller('IndexController', function($scope, $rootScope) {
+angular.module('app').controller('loginController', function($scope, $rootScope) {
     var email= "a@a.com";
     var password = "1234";
-    if ($login.email === email && $login.password === password){
-        alert("Success!");
-    }else {
-        alert("Fail!");
+   
+    function loginClicked() {
+
     }
 });
