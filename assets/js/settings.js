d4f66da247ee5d48b87dac322126975151d5321f
Dervis <dervis85@gmail.com>
2017-05-04 09:54:41 +0200

    added controller and settings state

---
+++ assets/js/settings.js
@@ -0,0 +1,13 @@
+angular.module('app').controller("settingsController", function ($scope) {
+
+$scope.saveSettings = function (userData) {
+    //Import userDBData
+
+    //DBUserEmail = userData.email;
+    //DBUserPassword = userData.password;
+    //DBUserProfileImage = userData.image;
+
+    }
+
+});
+
