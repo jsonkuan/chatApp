30567ce600d8438e06951d35fc92cc7d77aa5c7d
Jason Kuan <jsonkuan@gmail.com>
2017-05-08 11:08:44 +0200

    Change chat channel content

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -2,9 +2,18 @@ angular.module('app').controller('chatController', function($scope, $state, $sta
     $scope.channelId = $stateParams.channelId;
     $scope.channels = ['General', 'Work', 'Afterwork', 'Crazy cat videos', 'pr0n'];
     $scope.contacts = ['Snygg-Kuan', 'Cool-boy-Scolari', 'Papa-Niklas', 'Super-Jakob', 'Nerd-Dervish', 'Killer-Christian'];
+    $scope.message = {text: "Temp converation!"}; //TODO: MongoDB
 
     $scope.sendToSettings = function(){
         $state.transitionTo('settings');
     };
 
+    $scope.changeChannel = function(channel) {
+        var text = document.getElementById('channelContent');
+        text.innerHTML = $scope.message.text //TODO: Get from in MongoDB
+        console.log(text.innerHTML);
+    };
+    $scope.changeChannel($scope.channelId);
 });
+
+
