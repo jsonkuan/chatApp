9f45559e632b897ac0f79fe6461f06fd8e106838
Jakob Haglöf <j.haglof56@gmail.com>
2017-05-08 13:49:26 +0200

    Update chat.js
    Updated contact!
    
    Good commit!

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -2,7 +2,7 @@ angular.module('app').controller('chatController', function($scope, $rootScope, 
     $scope.channelId = $stateParams.channelId;
 
     $scope.channels = ['General', 'Work', 'Afterwork', 'Crazy cat videos', 'pr0n'];
-    $scope.contacts = ['Snygg-Kuan', 'Cool-boy-Scolari', 'Papa-Niklas', 'Super-Jakob', 'Nerd-Dervish', 'Killer-Christian'];
+    $scope.contacts = ['Snygg-Kuan', 'Cool-boy-Scolari', 'Papa-Niklas', 'Super Jakob?', 'Nerd-Dervish', 'Killer-Christian'];
 
     $scope.sendToSettings = function(){
         $state.transitionTo('settings');
