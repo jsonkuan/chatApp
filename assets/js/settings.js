2c70d5a63a8a784082922b4d53c0899fa9fa08c2
Jakob Haglöf <j.haglof56@gmail.com>
2017-05-08 15:45:10 +0200

    Able to change password and email

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,11 +1,14 @@
 angular.module('app').controller("settingsController", function ($scope, $state, $rootScope) {
 
     $scope.profileImage = "assets/images/defaultProfile.png";
-    //userData.password = $rootScope.activeUser.password;
+
+    $scope.userData = {email: "", password: "", userData: ""};
+    $scope.userData.email = $rootScope.activeUser.email;
+    $scope.userData.password = $rootScope.activeUser.password;
 
     $scope.saveSettings = function (userData) {
 
-        if(userData.password !== "") {
+        if(userData.password) {
 
             console.log($rootScope.activeUser.password);
 
@@ -15,11 +18,28 @@ angular.module('app').controller("settingsController", function ($scope, $state,
                     $rootScope.activeUser.password = userData.password;
                     $rootScope.users[i].password = userData.password;
 
-                    console.log("Users: " + $rootScope.users[i].password);
-                    console.log("ActiveUser: " + $rootScope.activeUser.password);
+                    console.log("Users password: " + $rootScope.users[i].password);
+                    console.log("ActiveUser password: " + $rootScope.activeUser.password);
                 }
+
             }
         }
+
+        if(userData.email) {
+
+            for(var i = 0; i < $rootScope.users.length; i++){
+                if($rootScope.activeUser.email === $rootScope.users[i].email) {
+
+                    $rootScope.activeUser.email = userData.email;
+                    $rootScope.users[i].email = userData.email;
+
+                    console.log("Users: email" + $rootScope.users[i].email);
+                    console.log("ActiveUser: email" + $rootScope.activeUser.email);
+                }
+            }
+
+        }
+
         $state.go("chat");
     };
 
