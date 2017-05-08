4384f7e0716a5417152d61c9c4a630799d9ae849
niklasbolwede <niklasbolwede@gmail.com>
2017-05-08 13:02:14 +0200

    Scroll to bottom with timer added

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -3,9 +3,11 @@ angular.module('app').run(function($rootScope){
 });
 
 
-angular.module('app').controller('chatController', function($scope, $state, $rootScope) {
+angular.module('app').controller('chatController', function($scope, $state, $rootScope, $timeout) {
     $scope.channels = ['General', 'Work', 'Afterwork', 'Crazy cat videos', 'pr0n'];
     $scope.contacts = ['Snygg-Kuan', 'Cool-boy-Scolari', 'Papa-Niklas', 'Super-Jakob', 'Nerd-Dervish', 'Killer-Christian'];
+
+
 
     $scope.sendToSettings = function(){
         $state.transitionTo('settings');
@@ -16,6 +18,11 @@ angular.module('app').controller('chatController', function($scope, $state, $roo
         $rootScope.allMessages.push(message);
         console.log($scope.allMessages.length);
     };
+
+    window.setInterval(function() {
+        var elem = document.getElementById('chat-text-box-container');
+        elem.scrollTop = elem.scrollHeight;
+    }, 1000);
 
     function formatDate() {
         var d1 = new Date();
@@ -31,22 +38,13 @@ angular.module('app').controller('chatController', function($scope, $state, $roo
 
     }
 
+
+
+
+
+
+
 });
 
 
-angular.module('app').directive('scrollBottom', function(){
-    return {
-        restrict: 'A',
-        scope: {scrollToBottom: "<"
-        },
-        link: function($scope, element){
-            $scope.$watchCollection('scrollToButton', function(newValue){
-                if (newValue){
-                    element.scrollToBottom = element.scrollHeight;
 
-                }
-            });
-        }
-    }
-
-});
