52a7df05fb9ebcf93a57e0f0cc9f46b2f0033391
Dervis <dervis85@gmail.com>
2017-05-12 22:43:10 +0200

    removes unused code

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,36 +1,41 @@
-angular.module('app').run(function($rootScope){
+angular.module('app').run(function($rootScope)
+{
     $rootScope.showReg = true;
     $rootScope.showUserName = false;
     $rootScope.showPasswordConfirm = false;
     $rootScope.showLoginButton = true;
     $rootScope.showRegButton = false;
-    //$rootScope.users = [];
-    $rootScope.user = {};
-    $rootScope.activeUser = {};
+    $rootScope.user = { };
+    $rootScope.activeUser = { };
 });
 
-angular.module('app').controller('loginController', function($scope, $rootScope, Authentication, $state, $filter, httpService) {
+angular.module('app').controller('loginController', function($scope, $rootScope, Authentication, $state, $filter, httpService)
+{
     var allUsers = httpService.getUsers();
     allUsers.then(function(data) {
         $rootScope.user = data.data;
     });
 
-
-
     $scope.loginButtonClicked = function() {
-        if(Authentication.login($scope.email, $scope.password)) {
+        if (Authentication.login($scope.email, $scope.password))
+        {
             $state.transitionTo('chat');
-        } else {
+        }
+        else
+        {
             $scope.error = '';
             $scope.email = '';
             $scope.password = '';
             alert("Incorrect!")
+
+
         }
     };
 
     $scope.registerButtonClicked = function() {
-        if(Authentication.register($scope.email, $scope.password, $scope.passwordConfirm)) {
-            var user = {email: $filter('lowercase')($scope.email), username: $scope.username, password: $scope.password, avatar:""};
+        if (Authentication.register($scope.email, $scope.password, $scope.passwordConfirm))
+        {
+            var user = { email: $filter('lowercase')($scope.email), username: $scope.username, password: $scope.password, avatar: ""};
             //$rootScope.users.push(user);
             shownElements();
             httpService.post(user);
@@ -42,10 +47,13 @@ angular.module('app').controller('loginController', function($scope, $rootScope,
 
         } else {
             alert("Incorrect!")
+
+
         }
     };
 
-    $scope.registerClicked = function() {
+    $scope.registerClicked = function()
+    {
         shownElements();
     };
 
@@ -58,41 +66,41 @@ angular.module('app').controller('loginController', function($scope, $rootScope,
     }
 });
 
-angular.module('app').factory('Authentication', function($rootScope) {
+angular.module('app').factory('Authentication', function($rootScope)
+{
     return {
-        login : function(inputEmail, inputPassword) {
+        login: function(inputEmail, inputPassword) {
             var isAuthenticated = false;
 
             for (var i = 0; i < $rootScope.user.length; i++){
-                if(inputEmail === $rootScope.user[i].email && inputPassword === $rootScope.user[i].password){
+                if (inputEmail === $rootScope.user[i].email && inputPassword === $rootScope.user[i].password){
                     var em = $rootScope.user[i].email;
                     var pw = $rootScope.user[i].password;
                     console.log(em + " " + pw);
                     isAuthenticated = true;
 
                     $rootScope.activeUser = $rootScope.user[i];
-                    console.log("input email and password  " + inputEmail +" "+ "  " + inputPassword + ":::  database data  " +
+                    console.log("input email and password  " + inputEmail + " " + "  " + inputPassword + ":::  database data  " +
                         $rootScope.user[i].email + "  " + $rootScope.user[i].password);
                 }
             }
 
             return isAuthenticated;
         },
-        register : function(inputEmail, inputPassword, passwordConfirm) {
+        register: function(inputEmail, inputPassword, passwordConfirm) {
             var isAuthenticated = false;
-            if (inputPassword === passwordConfirm){
-                if($rootScope.users.length > 0){
-                    for (var i = 0; i < $rootScope.users.length; i++){
-                        if(inputEmail === $rootScope.users[i].email){
-                            isAuthenticated = false;
-                        } else {
-                            isAuthenticated = true;
-                        }
+            if (inputPassword === passwordConfirm)
+            {
+                if ($rootScope.user.length > 0){
+                    for (var i = 0; i < $rootScope.user.length; i++){
+                        isAuthenticated = inputEmail !== $rootScope.user[i].email;
                     }
                 } else {
                     isAuthenticated = true;
                 }
-            } else {
+            }
+            else
+            {
                 isAuthenticated = false;
             }
             return isAuthenticated;
