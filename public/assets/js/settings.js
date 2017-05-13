38cc7df185ce0b05b43add8540e91a82f8b6d264
Dervis <dervis85@gmail.com>
2017-05-13 20:39:23 +0200

    undelets useful code

--- public/assets/js/settings.js
+++ public/assets/js/settings.js
@@ -1,51 +1,26 @@
-angular.module('app').controller("settingsController", function ($scope, $state, $rootScope, Upload, $timeout)
+angular.module('app').controller("settingsController", function ($scope, $state, $rootScope, Upload, httpService)
 {
 
     $scope.profileImage = "assets/images/defaultProfile.png";
-    $scope.userData = { email: "", password: "", username: ""};
-    $scope.userData.email = $rootScope.activeUser.email;
-    $scope.userData.password = $rootScope.activeUser.password;
-    $scope.userData.username = $rootScope.activeUser.username;
+     //$scope.userData.channels = $rootScope.activeUser.channels;
 
-    $scope.saveSettings = function(userData) {
-        if (userData.password)
-        {
-            console.log($rootScope.activeUser.password);
-
-            for (var i = 0; i < $rootScope.users.length; i++) {
-                if ($rootScope.activeUser.email === $rootScope.users[i].email) {
-
-                    $rootScope.activeUser.password = userData.password;
-                    $rootScope.users[i].password = userData.password;
-
-                    console.log("Users password: " + $rootScope.users[i].password);
-                    console.log("ActiveUser password: " + $rootScope.activeUser.password);
-                }
+        $scope.saveSettings = function () {
+            if ($scope.password) {
+                $rootScope.activeUser.password = $scope.password;
             }
-        }
 
-        if (userData.email)
-        {
-
-            for (var i = 0; i < $rootScope.users.length; i++){
-                if ($rootScope.activeUser.email === $rootScope.users[i].email) {
-
-                    $rootScope.activeUser.email = userData.email;
-                    $rootScope.users[i].email = userData.email;
-
-                    console.log("Users: email" + $rootScope.users[i].email);
-                    console.log("ActiveUser: email" + $rootScope.activeUser.email);
-                }
+            if($scope.email) {
+                $rootScope.activeUser.email = $scope.email;
             }
-            if (userData.username)
-            {
-                $rootScope.activeUser.username = userData.username;
+
+            if($scope.username){
+                $rootScope.activeUser.username = $scope.username;
             }
+            console.log("Settings");
+            console.log($rootScope.activeUser);
+            httpService.updateUser($rootScope.activeUser);
             $state.go("chat");
-        }
-    };
-
-
+        };
 
     $scope.uploadPic = function(file) {
         file.upload = Upload.upload({
