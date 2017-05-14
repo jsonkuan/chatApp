83d8dd2fe8cf153fb949e33e3c47c9d41528a5d0
Dervis <dervis85@gmail.com>
2017-05-14 22:27:01 +0200

    removed unused code

--- public/assets/js/settings.js
+++ public/assets/js/settings.js
@@ -1,31 +1,21 @@
 angular.module('app').controller("settingsController", function ($scope, $state, $rootScope, Upload, httpService)
 {
 
-    $scope.profileImage = "assets/images/defaultProfile.png";
-     //$scope.userData.channels = $rootScope.activeUser.channels;
+        //$scope.userData.channels = $rootScope.activeUser.channels;
 
-        $scope.saveSettings = function () {
-            if ($scope.password) {
-                $rootScope.activeUser.password = $scope.password;
-            }
+        $scope.saveSettings = function (activeUser) {
+            $rootScope.activeUser.password = activeUser.password;
+            $rootScope.activeUser.email = activeUser.email;
+            $rootScope.activeUser.username = activeUser.username;
 
-            if($scope.email) {
-                $rootScope.activeUser.email = $scope.email;
-            }
-
-            if($scope.username){
-                $rootScope.activeUser.username = $scope.username;
-            }
-            console.log("Settings");
-            console.log($rootScope.activeUser);
             httpService.updateUser($rootScope.activeUser);
             $state.go("chat");
         };
 
     $scope.uploadPic = function(file) {
         file.upload = Upload.upload({
-            url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-            data: { username: $scope.username, file: file}
+            url: 'public/assets/images',
+            data: {file: file}
         });
     }
 });
