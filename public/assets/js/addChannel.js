778d24cd81a9f857ffc71bd1e47d3b2897fbd287
Jason Kuan <jsonkuan@gmail.com>
2017-05-16 15:08:01 +0200

    Create new channel (Only name saves, page doesn't update)

--- public/assets/js/addChannel.js
+++ public/assets/js/addChannel.js
@@ -1,9 +1,17 @@
-angular.module('app').controller("channelController", function ($scope, $state, $rootScope, httpService) {
-        $scope.createChannel = function(activeUser) {
-            $rootScope.activeUser.channels = activeUser.channel;
+angular.module('app').controller("channelController", function ($scope, $state, $rootScope, channelService) {
+    $scope.createChannel = function(newChannel) {
 
-            //httpService.addChannelToDB();
-            $state.go("chat");
+        var channels = {
+            name: newChannel.channelName,
+            private: false,
+            user: [],
+            timestamp: ""
         };
+
+        channelService.post(channels).then(function(response) {
+            console.log("Channel service completed.");
+            $state.go("chat");
+        });
+    }
 });
 
