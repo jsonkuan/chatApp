50b4dc66ba963a10f67355a29256f1d158203c9c
niklasbolwede <niklasbolwede@gmail.com>
2017-05-16 15:37:20 +0200

    import angular smilies

--- public/assets/js/config.js
+++ public/assets/js/config.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ui.router', 'ngAnimate', 'ngMaterial']);
+var app = angular.module('app', ['ui.router', 'ngAnimate', 'ngMaterial', 'ngSanitize', 'ui.bootstrap', 'angular-smilies']);
 
 app.config(function($mdThemingProvider, $stateProvider, $qProvider, $urlRouterProvider) {
     $qProvider.errorOnUnhandledRejections(false);
