2e812f73fbb981919e21f9f54298c3b6c7edc9df
niklasbolwede <niklasbolwede@gmail.com>
2017-05-18 21:48:17 +0200

    adding klickevent to each user in chat.html and sends booth user id to start a new chat

--- public/assets/js/chat.js
+++ public/assets/js/chat.js
@@ -73,10 +73,16 @@ angular.module('app').controller('chatController', function($scope, $rootScope, 
     };
 
     $scope.startDirectChat = function(userA, userB) {
+        console.log("userA: " + userA );
+        console.log("userB: " + userB );
         channelService.get('/direct?sender=' + userA + '&recipient=' + userB).then(function(response) {
+            console.log("responseData: " +response);
             if (!response) {
+                console.log("creating new chat between userA: " +userA+" and userB: "+ userB);
                 $scope.createDirectChat(userA, userB);
             } else {
+                console.log("open old chat with id: "+response._id);
+                $scope.openChat(response._id);
                 //TODO open chat with response._id
             }
         });
