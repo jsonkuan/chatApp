6706868a5b9af351059bd915ce735a53010321dd
Dervis <dervis85@gmail.com>
2017-05-19 22:59:25 +0200

    fixes some avatar bugs and some other too

--- public/assets/js/settings.js
+++ public/assets/js/settings.js
@@ -3,7 +3,7 @@ angular.module('app').controller("settingsController", function ($scope, $state,
     $scope.password = userService.active.password ;
     $scope.email = userService.active.email;
     $scope.username = userService.active.username;
-    $scope.profileImage = userService.active.avatar;
+    $scope.avatar = userService.active.avatar;
 
     $scope.saveSettings = function () {
         userService.active.password = $scope.password;
@@ -15,12 +15,12 @@ angular.module('app').controller("settingsController", function ($scope, $state,
                 url: '/upload',
                 method: 'POST',
                 data: {
-                    avatar: $scope.avatar // a jqLite type="file" element, upload() will extract all the files from the input and put them into the FormData object before sending.
+                    avatar: $scope.avatar
                 }
             }).then(
                 function (response) {
                     userService.active.avatar = response.data.slice(7);
-                    $scope.profileImage = userService.active.avatar;
+                    $scope.avatar = userService.active.avatar;
                     userService.updateUser(userService.active);
                 },
                 function (response) {
