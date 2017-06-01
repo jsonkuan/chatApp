469c5cd1931a850fc16d00b31dbf7c7327ad6c87
Jakob Haglöf <j.haglof56@gmail.com>
2017-06-01 15:25:42 +0200

    loops through users in database and adds name to sidemenu

--- mobile/www/js/chat.js
+++ mobile/www/js/chat.js
@@ -1,9 +1,13 @@
-app.controller('chatController', function($scope, $ionicSideMenuDelegate) {
+app.controller('chatController', function($scope, $ionicSideMenuDelegate, userService) {
+
+  $scope.users = [];
 
      $scope.toggleLeft = function() {
        $ionicSideMenuDelegate.toggleLeft();
      };
 
-
-
+    userService.getUsers().then(function(result){
+    $scope.users = result;
+    console.log(result);
+  })
 });
