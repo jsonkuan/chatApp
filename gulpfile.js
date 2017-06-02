1bc832f7d5783de1d41c7124221813bed3f22003
Christian Blomqvist <gaspop@MacBook-Pro.local>
2017-06-02 13:28:23 +0200

    Gulp watches index, common and images.

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,30 @@ var gulp = require('gulp');
 var live = require('gulp-live-server');
 var watch = require('gulp-watch');
 
-gulp.task('serve', function(){
+gulp.task('watchServer', function() {
     var server = live.new('index.js');
     server.start();
 
-    gulp.watch('index.js', function(){
+    gulp.watch('index.js', function() {
         server.start.apply(server);
     });
-
-    gulp.watch('common/common.js', function(){
-        gulp.src('./common/common.js').pipe(gulp.dest('./webb'));
-        gulp.src('./common/common.js').pipe(gulp.dest('./mobile/www'));
-        server.start.apply(server);
-    });
-
-    watch('./common/images/**/*.{ttf,woff,eof,svg,png,jpeg}', function(){
-        gulp.src('./common/images/**/*.{ttf,woff,eof,svg,png}').pipe(gulp.dest('./mobile/www/img'));
-        gulp.src('./common/images/**/*.{ttf,woff,eof,svg,png}').pipe(gulp.dest('./webb/assets/images'));
-        server.start.apply(server);
-    });
-
 });
 
+gulp.task('watchCommon', function() {
+    gulp.watch('common/common.js', function() {
+        gulp.src('common/common.js')
+        .pipe(gulp.dest('webb'))
+        .pipe(gulp.dest('mobile/www'));
+    });    
+});
 
+gulp.task('watchImages', function() {
+    var path = 'common/images/*.{gif,jpg,jpeg,png,svg}';
+    return watch([path], { ignoreInitial: false }, function() {
+        gulp.src(path)
+        .pipe(gulp.dest('webb/assets/images'))
+        .pipe(gulp.dest('mobile/www/img'));
+    });
+});
+
+gulp.task('default', ['watchServer', 'watchCommon', 'watchImages']);
