0452646c7596a42f945abbaca575104d81df5d05
Dervis <dervis85@gmail.com>
2017-06-08 00:28:59 +0200

    bug fixes, styling, online/offline, current user in left drawer, shows user avatar

--- mobile/www/js/app.js
+++ mobile/www/js/app.js
@@ -6,97 +6,97 @@
 var app = angular.module('starter', ['ionic', 'common', 'ngMessages', 'ngFileUpload', 'lr.upload', 'ngCordova']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('login');
-    $stateProvider
+  $urlRouterProvider.otherwise('login');
+  $stateProvider
     .state('login', {
-            url: '/login',
-            controller : 'loginController',
-            templateUrl: 'partials/login.html',
-            resolve: {
-                session: function(Resolvers) {
-                    return Resolvers.getUser();
-                }
-            }
-        })
-        .state('chat', {
-            url:'/chat',
-            controller: 'chatController',
-            templateUrl: 'partials/chat.html',
-            resolve: {
-                session: function(Resolvers) {
-                    return Resolvers.getUser();
-                },
-                currentChannel: function(session, Resolvers) {
-                    return Resolvers.getChannel();
-                },
-                userChannels: function(session, currentChannel, channelService) {
-                    return channelService.getChannelsForUser(session._id);
-                },
-                userContacts: function(session, userService){
-                    return userService.getUsers();
-                }
-            }
-        })
-        .state('settings', {
-            url: '/settings',
-            controller: 'settingsController',
-            templateUrl: 'partials/settings.html',
-            resolve: {
-                session: function(Resolvers) {
-                    return Resolvers.getUser();
-                }
-            }
-        })
-        .state('addChannel', {
-            url: '/addChannel',
-            controller: 'channelController',
-            templateUrl: 'partials/addChannel.html',
-            resolve: {
-                session: function(Resolvers) {
-                    return Resolvers.getUser();
-                },
-                allUsers: function(userService){
-                    return userService.getUsers();
-                }
-            }
-        });
+      url: '/login',
+      controller : 'loginController',
+      templateUrl: 'partials/login.html',
+      resolve: {
+        session: function(Resolvers) {
+          return Resolvers.getUser();
+        }
+      }
+    })
+    .state('chat', {
+      url:'/chat',
+      controller: 'chatController',
+      templateUrl: 'partials/chat.html',
+      resolve: {
+        session: function(Resolvers) {
+          return Resolvers.getUser();
+        },
+        currentChannel: function(session, Resolvers) {
+          return Resolvers.getChannel();
+        },
+        userChannels: function(session, currentChannel, channelService) {
+          return channelService.getChannelsForUser(session._id);
+        },
+        userContacts: function(session, userService){
+          return userService.getUsers();
+        }
+      }
+    })
+    .state('settings', {
+      url: '/settings',
+      controller: 'settingsController',
+      templateUrl: 'partials/settings.html',
+      resolve: {
+        session: function(Resolvers) {
+          return Resolvers.getUser();
+        }
+      }
+    })
+    .state('addChannel', {
+      url: '/addChannel',
+      controller: 'channelController',
+      templateUrl: 'partials/addChannel.html',
+      resolve: {
+        session: function(Resolvers) {
+          return Resolvers.getUser();
+        },
+        allUsers: function(userService){
+          return userService.getUsers();
+        }
+      }
+    });
 });
 
 app.run(function($ionicPlatform, $state, $rootScope, Resolvers) {
-    $ionicPlatform.ready(function() {
+  $ionicPlatform.ready(function() {
 
-        if(window.cordova && window.cordova.plugins.Keyboard) {
-            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-            // for form inputs)
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-            // Don't remove this line unless you know what you are doing. It stops the viewport
-            // from snapping when text inputs are focused. Ionic handles this internally for
-            // a much nicer keyboard experience.
-            cordova.plugins.Keyboard.disableScroll(true);
-        }
-        if(window.StatusBar) {
-            StatusBar.styleDefault();
-        }
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
 
+  });
+
+  $rootScope.$on('$stateChangeStart', function(event, to) {
+    var destination = to.name;
+    Resolvers.getUser().then(function(response) {
+      var user = response;
+      //Block access outside of login when not logged in
+      if (!user && destination !== "login") {
+        console.log('nein!');
+        event.preventDefault();
+        $state.go('login');
+        //Block access to login when not logged in
+      } else if (user && destination === "login") {
+        event.preventDefault();
+        //TODO: Send back to previous state instead of 'chat
+        $state.go('chat');
+      }
     });
-
-    $rootScope.$on('$stateChangeStart', function(event, to) {
-        var destination = to.name;
-        Resolvers.getUser().then(function(response) {
-            var user = response;
-            //Block access outside of login when not logged in
-
-            if (!user && destination !== "addChannel") {
-                event.preventDefault();
-                $state.go('addChannel');
-            //Block access to login when not logged in
-            } else if (user && destination === "addChannel") {
-                event.preventDefault();
-                //TODO: Send back to previous state instead of 'chat
-                $state.go('addChannel');
-            }
-        });
-    });
+  });
 
 });
--- mobile/www/js/login.js
+++ mobile/www/js/login.js
@@ -1,119 +1,110 @@
 app.controller('loginController', function($scope, $state, $filter, userService) {
-    $scope.showReg = true;
-    $scope.showUserName = false;
-    $scope.showPasswordConfirm = false;
-    $scope.showLoginButton = true;
-    $scope.showRegButton = false;
-    $scope.showCancel = false;
-    $scope.isAuthenticated = true;
-    $scope.placeHolder = "Email";
-    $scope.user = {};
-  
-    userService.post({
+  $scope.showReg = true;
+  $scope.showUserName = false;
+  $scope.showPasswordConfirm = false;
+  $scope.showLoginButton = true;
+  $scope.showRegButton = false;
+  $scope.showCancel = false;
+  $scope.isAuthenticated = true;
+  $scope.placeHolder = "Email";
+  $scope.user = {};
+
+  userService.get('133333333333333333333337').then(function(response) {
+    if (!response) {
+      userService.post({
         _id: "133333333333333333333337",
         username: "SnakkBot",
         email: "bot@snakk.com",
         password: "2017",
         avatar: "img/snakk-bot.jpg",
         status: "offline"
+      });
+    }
+  });
 
+  userService.getUsers().then(function(response) {
+    $scope.user = response;
+  });
 
-    userService.get('133333333333333333333337').then(function(response) {
-        if (!response) {
-            userService.post({
-                _id: "133333333333333333333337",
-                username: "SnakkBot",
-                email: "bot@snakk.com",
-                password: "2017",
-                avatar: "img/snakk-bot.jpg",
-                status: "offline"
-            });
-        }
-    });
-
+  $scope.loginButtonClicked = function() {
     userService.getUsers().then(function(response) {
-        $scope.user = response;
+      $scope.user = response;
     });
+    if ($scope.login($scope.email, $scope.password)) {
+      /*if(userService.active.status === "online"){
+       $scope.placeHolder = "You are already logged in";
+       }else {*/
+      userService.active.status = "online";
+      userService.updateUser(userService.active);
+      //NOTE: Store user id as cookie on login.
+      localStorage['user'] = userService.active._id;
+      $state.transitionTo('chat');
+      //}
+    } else {
+      $scope.password = "";
+    }
+  };
 
-    $scope.loginButtonClicked = function() {
-        userService.getUsers().then(function(response) {
+  $scope.registerButtonClicked = function() {
+    $scope.isAuthenticated = true;
+    if($scope.username && $scope.email && $scope.password && $scope.confirm){
+      if ($scope.register($scope.email, $scope.password, $scope.confirm)){
+        var user = { email: $filter('lowercase')($scope.email), username: $scope.username,
+          password: $scope.password, avatar: "img/defaultProfile.png", status: "offline", warnings: 0};
+        shownElements();
+        $scope.email = $scope.email.toLowerCase();
+        userService.post(user).then(function(response) {
+          userService.getUsers().then(function(response) {
             $scope.user = response;
+          });
         });
-        if ($scope.login($scope.email, $scope.password)) {
-            /*if(userService.active.status === "online"){
-                $scope.placeHolder = "You are already logged in";
-            }else {*/
-                userService.active.status = "online";
-                userService.updateUser(userService.active);
-                //NOTE: Store user id as cookie on login.
-                localStorage['user'] = userService.active._id;
-                $state.transitionTo('chat');
-            //}
-        } else {
-            $scope.password = "";
-        }
-    };
+      }
+    }else{
 
-    $scope.registerButtonClicked = function() {
+    }
+  };
+
+  $scope.registerClicked = function() {
+    shownElements();
+  };
+
+  $scope.cancelClicked = function() {
+    shownElements();
+  };
+
+  var shownElements = function() {
+    $scope.showReg = !$scope.showReg;
+    $scope.showLoginButton = !$scope.showLoginButton;
+    $scope.showRegButton = !$scope.showRegButton;
+    $scope.showPasswordConfirm = !$scope.showPasswordConfirm;
+    $scope.showUserName = !$scope.showUserName;
+    $scope.showCancel = !$scope.showCancel;
+  };
+
+  $scope.login = function(inputEmail, inputPassword) {
+    console.log($scope.user.length);
+    for (var i = 0; i < $scope.user.length; i++) {
+      if (inputEmail === $scope.user[i].email && inputPassword === $scope.user[i].password) {
         $scope.isAuthenticated = true;
-        if($scope.username && $scope.email && $scope.password && $scope.confirm){
-            if ($scope.register($scope.email, $scope.password, $scope.confirm)){
-                var user = { email: $filter('lowercase')($scope.email), username: $scope.username,
-                    password: $scope.password, avatar: "img/defaultProfile.png", status: "offline", warnings: 0};
-                shownElements();
-                $scope.email = $scope.email.toLowerCase();
-                userService.post(user).then(function(response) {
-                  userService.getUsers().then(function(response) {
-                    $scope.user = response;
-                  });
-                });
-            }
-        }else{
-
-        }
-    };
-
-    $scope.registerClicked = function() {
-        shownElements();
-    };
-
-    $scope.cancelClicked = function() {
-        shownElements();
-    };
-
-    var shownElements = function() {
-        $scope.showReg = !$scope.showReg;
-        $scope.showLoginButton = !$scope.showLoginButton;
-        $scope.showRegButton = !$scope.showRegButton;
-        $scope.showPasswordConfirm = !$scope.showPasswordConfirm;
-        $scope.showUserName = !$scope.showUserName;
-        $scope.showCancel = !$scope.showCancel;
-    };
-
-    $scope.login = function(inputEmail, inputPassword) {
-        console.log($scope.user.length);
-        for (var i = 0; i < $scope.user.length; i++) {
-            if (inputEmail === $scope.user[i].email && inputPassword === $scope.user[i].password) {
-                $scope.isAuthenticated = true;
-                userService.active = $scope.user[i];
-                return $scope.isAuthenticated;
-            }
-        }
-    };
-    $scope.register = function(inputEmail, inputPassword, passwordConfirm) {
-        console.log(inputPassword, passwordConfirm);
-        if (inputPassword === passwordConfirm) {
-            for (var i = 0; i < $scope.user.length; i++){
-                if(inputEmail === $scope.user[i].email)
-                {
-                    $scope.isAuthenticated = false;
-                    $scope.email = "";
-                    $scope.placeHolder = "User exists";
-                }
-            }
-        } else {
-            $scope.isAuthenticated = false;
-        }
+        userService.active = $scope.user[i];
         return $scope.isAuthenticated;
-    };
+      }
+    }
+  };
+  $scope.register = function(inputEmail, inputPassword, passwordConfirm) {
+    console.log(inputPassword, passwordConfirm);
+    if (inputPassword === passwordConfirm) {
+      for (var i = 0; i < $scope.user.length; i++){
+        if(inputEmail === $scope.user[i].email)
+        {
+          $scope.isAuthenticated = false;
+          $scope.email = "";
+          $scope.placeHolder = "User exists";
+        }
+      }
+    } else {
+      $scope.isAuthenticated = false;
+    }
+    return $scope.isAuthenticated;
+  };
 });
