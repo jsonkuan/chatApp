cd4d13230eb59d4e82ac51ad188b80b6600f9b69
guasc0 <gual.scolari@hotmail.com>
2017-06-12 14:39:28 +0200

    minor fixes

--- mobile/www/js/addChannel.js
+++ mobile/www/js/addChannel.js
@@ -7,15 +7,11 @@ app.controller("channelController", function ($scope, $state, userService, chann
     $scope.usersWithoutBot = $scope.users;
     $scope.invitedUsers = [userService.active._id];
 
-    console.log(userService.active.username);
-
-    $scope.publicOrPrivate = function () {
+    $scope.publicOrPrivate = function() {
         $scope.accessability = "private";
-        console.log($scope.accessability);
     }
-    //Just a test function to post channels to db
-    $scope.createChannel2 = function () {
 
+    $scope.createChannel = function() {
         var channels = {
             name: $scope.channelName.text,
             purpose: $scope.purpose.text,
@@ -26,43 +22,39 @@ app.controller("channelController", function ($scope, $state, userService, chann
         channelService.post(channels).then(function (response) {
             $state.go("chat");
         });
-        console.log(channels);
     }
-    $scope.resetChannelname = function () {
+
+    $scope.resetChannelname = function() {
         $scope.channelName.text = "";
-        console.log("PÅKE");
     };
 
-    $scope.resetPurpose = function () {
+    $scope.resetPurpose = function() {
         $scope.purpose.text = "";
-        console.log("PÅKE");
     };
 
     userService.getUsers().then(function (response) {
         $scope.users = response;
     });
 
-    $scope.backToChat = function () {
+    $scope.backToChat = function() {
         $state.go('chat');
     }
 
-    $scope.addToChannel = function (user) {
+    $scope.addToChannel = function(user) {
         if(!$scope.invitedUsers.contains(user.username)) {
-            $scope.invitedUsers.push(user.username);
-            console.log("Added " + user.username);
+            $scope.invitedUsers.push(user._id);
         } else {
             var index = $scope.invitedUsers.indexOf(user.username);            
             $scope.invitedUsers.splice(index, 1);
-            console.log("removed user from Index:" + index);
         }
     };
 
-    $scope.removeFromChannel = function (index) {
+    $scope.removeFromChannel = function(index) {
         $scope.invitedUsers.splice(index, 1);
         console.log("Removed");
     };
 
-    $scope.filterInvitedUsers = function (user) {
+    $scope.filterInvitedUsers = function(user) {
         return ($scope.invitedUsers.contains(user));
     }
 });
@@ -74,4 +66,4 @@ Array.prototype.contains = function contains(obj) {
         }
     }
     return false;
-};
+}; 
