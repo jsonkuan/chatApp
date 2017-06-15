3714c0c64ab1d12c59c664634c918ed6f89a97af
Dervis <dervis85@gmail.com>
2017-06-15 14:44:13 +0200

    fixes img for localhost

--- mobile/www/js/chat.js
+++ mobile/www/js/chat.js
@@ -372,7 +372,8 @@ app.controller('chatController', function ($scope, $state, $ionicSideMenuDelegat
 
     if ($scope.avatar != userService.active.avatar && $scope.userInput.avatar !== "") {
       upload({
-        url: '/upload',
+        //use /upload for server
+        url: 'http://localhost:3000/upload',
         method: 'POST',
         data: {
           avatar: $scope.userInput.avatar
@@ -402,7 +403,8 @@ app.controller('chatController', function ($scope, $state, $ionicSideMenuDelegat
 
     if ($scope.chatInput.attachment) {
       upload({
-        url: '/upload',
+        //use /upload for server
+        url: 'http://localhost:3000/upload',
         method: 'POST',
         data: {
           avatar: $scope.chatInput.attachment
