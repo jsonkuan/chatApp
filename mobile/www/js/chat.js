2b44421fa8a76f4c8dd6787130e8f49386393b6a
Dervis <dervis85@gmail.com>
2017-06-19 09:47:43 +0200

    shows chat input on typing

--- mobile/www/js/chat.js
+++ mobile/www/js/chat.js
@@ -17,7 +17,7 @@ app.controller('chatController', function ($scope, $state, $ionicSideMenuDelegat
   $scope.warning = false;
   $scope.intervals = [];
   $scope.host = REST.host + '/';
-  //window.cordova.plugins.Keyboard.disableScroll(false);
+  window.cordova.plugins.Keyboard.disableScroll(false);
 
   //returns top users and number of posts list.
   $scope.addUsersToPosters = function(topList, users){
